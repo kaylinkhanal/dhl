546449a96f7cf87bc1845ab8a3f98d7c8dc4653e
kaylinkhanal10 <kaylin.k@foundationai.com>
2022-12-28 08:40:56 +0545

    changed naming

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import Register from "./conatiners/registration/register";
+import Register from "./containers/registration/register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
