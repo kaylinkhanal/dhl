b7ddb39d4523933235d0f45e9ea47971731f4d60
sonamLama22 <lamadsonam19@gmail.com>
2022-12-28 22:03:57 -0500

    added a login form

--- Server/src/models/users.js
+++ Server/src/models/users.js
@@ -1,9 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const usersSchema = new Schema({
-    name: {type:String, unique: true},
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    password: { type: String, required: true },
+    userRole: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+    temporaryAddress: { type: String, required: true },
+    country: { type: String, required: true },
+    zipCode: { type: Number, required: true },
   },
-  { collection: 'users' });
+  { collection: "users" }
+);
 
-module.exports= mongoose.model('Users', usersSchema);
+module.exports = mongoose.model("Users", usersSchema);
--- Server/src/routes/userRouter.js
+++ Server/src/routes/userRouter.js
@@ -1,10 +1,23 @@
-const { Router } = require('express');
-const Users = require('../models/users')
+const { Router } = require("express");
+const Users = require("../models/users");
 const app = Router();
+const bcrypt = require("bcrypt");
 
-app.get('/users', async(req, res) => {
- 
-})
-
+app.post("/register", async (req, res) => {
+  try {
+    const data = await Users.create(req.body);
+    if (data) {
+      res.json({
+        msg: "user is registered",
+      });
+    } else {
+      res.json({
+        msg: "something went wrong",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = app;
