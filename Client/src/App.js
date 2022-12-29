a5d13deb3640e67fdbe9da6db307519c57eea8a4
Deepak Dahal <Deepak9dahal@gmail.com>
2022-12-29 08:25:29 +0545

    first try

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './App.css';
+import Register from "./containers/registration/register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./containers/login/login";
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Client/src/containers/login/login.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as Yup from "yup";
+import "../style.css";
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .min(5, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
+
+const Login = () => {
+  const [message, setMessage] = useState("");
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={loginSchema}
+        onSubmit={(values, { resetForm }) => {
+          // console.log(values)
+
+          const requestOptions = {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(values),
+          };
+          fetch("http://localhost:5000/login", requestOptions)
+            .then((res) => res.json())
+            .then((data) => setMessage(data.msg));
+          resetForm();
+        }}
+      >
+        {({ errors, touched }) => (
+          <div className="register">
+            <Form>
+              <Field name="email" type="email" placeholder="Enter your email" />
+              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+              <Field
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+              />
+              {errors.password && touched.password ? (
+                <div>{errors.password}</div>
+              ) : null}
+
+              <button className="btn btn-success" type="submit">
+                Login
+              </button>
+              <h6> {message} </h6>
+            </Form>
+          </div>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Login;
