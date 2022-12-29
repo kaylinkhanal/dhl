359d608231db946e49f5ede37219ac4364d68c0c
Sushant9818 <prasusan123@gmail.com>
2022-12-28 21:25:26 -0800

    added registration and login

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './App.css';
+import Register from "./containers/register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./containers/login";
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
