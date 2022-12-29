61fe3217245e0096658302669a09a0325480adc1
kaylinkhanal10 <kaylin.k@foundationai.com>
2022-12-29 13:04:00 +0545

    Add comments

--- Client/src/index.js
+++ Client/src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
