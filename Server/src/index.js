f1d52b5514aa4a03b1eaf7825f21a25f7bae4319
bpunam967 <you@example.com>
2023-01-02 08:13:03 +0545

    test

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,12 +3,16 @@ require('dotenv').config()
 const port = process.env.PORT
 const cors = require('cors')
 const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(cors())
+
 const connect = require('./db/connect')
 connect()
-const user = require('./routes/userRouter');
-app.use(user)
+
+app.use(bodyParser.json())
+app.use(cors())
+const registerRouter = require('./routes/registerRouter');
+const loginRouter = require('./routes/loginRouter');
+app.use(registerRouter)
+app.use(loginRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
