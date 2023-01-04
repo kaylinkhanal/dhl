d5b621a1d0141f89ee9acfbb0916a7fc14319073
Ambica <ambicana5@gmail.com>
2023-01-05 03:10:44 +0545

    admin panel setup

--- Client/src/containers/Admin/dashboard.js
+++ Client/src/containers/Admin/dashboard.js
@@ -1,9 +1,16 @@
-import React, {useState} from "react";
-import { Button, Drawer } from 'antd';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Dashboard = ()=>{
+    const {name} = useSelector(state=> state.user)
     return(
-        <></>
+        <section id="admin_dashboard">
+            <div className="container">
+                <div className="main_content">
+                    <h1>Welcome, {name}</h1>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Dashboard
