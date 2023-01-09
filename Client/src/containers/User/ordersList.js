00101860194415c32b3a453d3e088da50461d9de
unknown <sagarmogar5@gmail.com>
2023-01-09 19:10:12 +0545

    added skeleton

--- Client/src/containers/User/ordersList.js
+++ Client/src/containers/User/ordersList.js
@@ -1,38 +1,46 @@
-import React, {useEffect, useState} from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Box from '../../components/box'
-import { faDolly } from '@fortawesome/free-solid-svg-icons'
-const OrdersList = ()=>{
-    const [orderList, setOrderList] = useState([])
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Box from "../../components/box";
+import { faDolly } from "@fortawesome/free-solid-svg-icons";
+import { Skeleton } from "antd";
 
-    const fetchData = async()=>{
-        const response = await fetch("http://localhost:5000/orders")
-        const data = await response.json()
+const OrdersList = () => {
+  const [orderList, setOrderList] = useState([]);
 
-        if(data){
-            setOrderList(data.ordersList)
-        }
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:5000/orders");
+    const data = await response.json();
+
+    if (data) {
+      setOrderList(data.ordersList);
     }
-  
+  };
 
-    useEffect(()=>{
-        fetchData()
-    },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return(
-        <section>
-            <div className='container'>
-                <div className='orderList'>
-                    <h1 className='title'><i><FontAwesomeIcon icon={faDolly}/></i> My Orders</h1>
-                    {orderList.length > 0 ? orderList.map((item)=>{
-                        return(
-                            <Box item={item} fetchData={fetchData}/>
-                            )
-                    }): 'list not found'}
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <div className="container">
+        <div className="orderList">
+          <h1 className="title">
+            <i>
+              <FontAwesomeIcon icon={faDolly} />
+            </i>{" "}
+            My Orders
+          </h1>
+          {orderList.length > 0 ? (
+            orderList.map((item) => {
+              return <Box item={item} fetchData={fetchData} />;
+            })
+          ) : (
+            <Skeleton />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default OrdersList
+export default OrdersList;
