16955d219ed9aaf15b6403ba90402a38711613fb
sonamLama22 <lamadsonam19@gmail.com>
2023-01-13 01:39:19 -0500

    added confirm delete pop up

--- Client/src/components/cardSkeletion.js
+++ Client/src/components/cardSkeletion.js
@@ -1,11 +1,11 @@
-import React,{useState} from 'react'
-import { Skeleton } from 'antd'
+import React, { useState } from "react";
+import { Skeleton } from "antd";
 
-const CardSkeletion = (props)=>{
-    const [active, setActive] = useState(false);
-  const [size, setSize] = useState('small');
-  const [buttonShape, setButtonShape] = useState('round');
-  const [avatarShape, setAvatarShape] = useState('circle');
+const CardSkeletion = (props) => {
+  const [active, setActive] = useState(false);
+  const [size, setSize] = useState("small");
+  const [buttonShape, setButtonShape] = useState("round");
+  const [avatarShape, setAvatarShape] = useState("circle");
   const handleActiveChange = (checked) => {
     setActive(checked);
   };
@@ -15,43 +15,63 @@ const CardSkeletion = (props)=>{
   const handleAvatarShape = (e) => {
     setAvatarShape(e.target.value);
   };
-    return(
-        Array(props.boxNumber).fill(0).map((item, id)=>{
-            return(
-                <div key={id}>
-                    <div className='btns'>
-                        <button><Skeleton.Avatar active size={size} shape={avatarShape} /></button> 
-                        <button><Skeleton.Avatar active size={size} shape={avatarShape} /></button>
-                    </div>
-                    <div className='loader order_item'>
-                        <div className='top'>
-                                <p className='badge'><Skeleton.Input active size={size}/></p>
-                                <span className='orderId'><Skeleton.Input active size={size}/></span> 
-                                <span><Skeleton.Input active size={size}/></span>
-                            </div>
-                            <div className='bottom'>
-                                <ul>
-                                    <li>
-                                        <i><Skeleton.Button active size={size} shape={buttonShape} /></i> 
-                                    </li>
-                                    <li>
-                                        <i><Skeleton.Button active size={size} shape={buttonShape} /></i> 
-                                    </li>
-                                    <li>
-                                        <i><Skeleton.Button active size={size} shape={buttonShape} /></i> 
-                                    </li>
-                                    <li>
-                                        <i><Skeleton.Button active size={size} shape={buttonShape} /></i> 
-                                    </li>
-                                    <li>
-                                        <i><Skeleton.Button active size={size} shape={buttonShape} /></i> 
-                                    </li>
-                                </ul>
-                            </div>
-                    </div>
-                </div>
-            )
-        })
-    )
-}
-export default CardSkeletion
+  return Array(props.boxNumber)
+    .fill(0)
+    .map((item, id) => {
+      return (
+        <div key={id}>
+          <div className="btns">
+            <button>
+              <Skeleton.Avatar active size={size} shape={avatarShape} />
+            </button>
+            <button>
+              <Skeleton.Avatar active size={size} shape={avatarShape} />
+            </button>
+          </div>
+          <div className="loader order_item">
+            <div className="top">
+              <p className="badge">
+                <Skeleton.Input active size={size} />
+              </p>
+              <span className="orderId">
+                <Skeleton.Input active size={size} />
+              </span>
+              <span>
+                <Skeleton.Input active size={size} />
+              </span>
+            </div>
+            <div className="bottom">
+              <ul>
+                <li>
+                  <i>
+                    <Skeleton.Button active size={size} shape={buttonShape} />
+                  </i>
+                </li>
+                <li>
+                  <i>
+                    <Skeleton.Button active size={size} shape={buttonShape} />
+                  </i>
+                </li>
+                <li>
+                  <i>
+                    <Skeleton.Button active size={size} shape={buttonShape} />
+                  </i>
+                </li>
+                <li>
+                  <i>
+                    <Skeleton.Button active size={size} shape={buttonShape} />
+                  </i>
+                </li>
+                <li>
+                  <i>
+                    <Skeleton.Button active size={size} shape={buttonShape} />
+                  </i>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    });
+};
+export default CardSkeletion;
