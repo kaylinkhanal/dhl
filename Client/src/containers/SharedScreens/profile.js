2e20eea62c8aa15392952b32333aa11296efea36
Ambica <ambicana5@gmail.com>
2023-01-19 02:08:18 +0545

    added skeleton on first load

--- Client/src/containers/SharedScreens/profile.js
+++ Client/src/containers/SharedScreens/profile.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { FaCamera, FaPencilAlt } from "react-icons/fa";
-import { useParams } from "react-router-dom";
+import { message, Skeleton } from "antd";
 import axios from 'axios';
 import {useSelector} from 'react-redux'
+import DummyAvatar from '../../images/dummy_img.png'
+
 const Portfolio =()=>{
     const {_id} = useSelector(state=> state.user)
     const [isUploaded, setIsUploaded] = useState(true)
     const [userDetails, setUserDetails] = useState({})
+    const [loading, setLoading] = useState(false)
+    // const userImg = DummyAvatar
+
     const avatarupload  = async (file) => {
         setIsUploaded(false)
         const formData = new FormData();
@@ -16,20 +21,21 @@ const Portfolio =()=>{
             body: formData,
         })
         if(data){
-            alert("uploaded")
+            message.success("uploaded")
             setIsUploaded(true)
         }
 
     }
-   //task
-   // useEffect->  fetch -> localhost:5000/profile/63bb8b2fa2db5d18a67d744b
-   //-> data.user.avatarFileName
-   //save it into a state
     const fetchUserProfileDetails = () => {
         axios.get(`${process.env.REACT_APP_BASE_URL}/profile/${_id}`)
-        .then(res=> setUserDetails(res.data.user))
+        .then(res=> {
+            setUserDetails(res.data.user)
+            if(res.data.user){
+                setLoading(true)
+            }
+        })
+        
     }
-
    useEffect(()=>{
         fetchUserProfileDetails()
    },[isUploaded])
@@ -40,7 +46,10 @@ const Portfolio =()=>{
             <div className="container">
                 <div className="user_profile">
                     <div className="user_img">
-                        <img src={require(`../../../src/uploads/${userDetails.avatarFileName || 'card_img.jpg'}`).default} alt="profile" height={'100%'} width={'100%'}/>
+                        {loading?
+                        <img src={
+                            require(`../../../src/uploads/${userDetails.avatarFileName || 'card_img.jpg'}`).default}alt="profile"  height={'100%'} width={'100%'}
+                        />: <Skeleton.Avatar active size={200}/>}
                         
                         <div className="uploader">
                             <input onChange={(e)=> avatarupload(e.target.files[0])} type="file" id="upload" hidden/>
