194057468aad7e2b81adacc00b1130b753fd05d9
Prasiddha <shakyaprasiddha8@gmail.com>
2023-01-19 08:23:07 +0545

    response handler

--- Client/src/utils/responseHandler.js
+++ Client/src/utils/responseHandler.js
@@ -1,7 +1,7 @@
 export const responseHandler=(response)=>{
-     if(response.status== 401){
+     if(response.status=== 401){
         return "invalid credentials"
-      }else if(response.status == 200){
+      }else if(response.status === 200){
         return "change password success"
       }else{
           return "something went wrong"
