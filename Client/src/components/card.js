af64585661f8ce3fc90a46530e016a2521069c84
Prasiddha <shakyaprasiddha8@gmail.com>
2023-01-25 15:38:30 +0545

    changes

--- Client/src/components/card.js
+++ Client/src/components/card.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-// library.add(
-//     FaDolly,
-//     FaTelegramPlane
-// );
 
 const Card = ({icon, title, link})=>{
     return(
